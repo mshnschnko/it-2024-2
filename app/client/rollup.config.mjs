6209cebc6b3f3ececfe8c8f28d3f9ededd90e08d
Denis Ivanov <denis.ivanov@mail.ru>
2025-02-08 15:08:45 +0300

    начальное состояние проекта

---
+++ app/client/rollup.config.mjs
@@ -0,0 +1,18 @@
+import babel from '@rollup/plugin-babel';
+const pages = ['hello.js'];
+
+const pluginList = [babel({ babelHelpers: 'bundled' })];
+const export_page = pages.reduce((acc, item) => {
+    acc.push({
+        input: `./src/${item}`,
+        output: {
+            file: `../server/www/js/${item}`,
+            format: 'cjs',
+            sourcemap: 'inline',
+        },
+        plugins: pluginList,
+    });
+    return acc;
+}, []);
+
+export default export_page;
---
+++ app/client/src/hello.js
@@ -0,0 +1,6 @@
+import { mount, el } from "../node_modules/redom/dist/redom.es";
+
+mount(
+    document.getElementById("main"),
+    <h1>Hello</h1>
+);
